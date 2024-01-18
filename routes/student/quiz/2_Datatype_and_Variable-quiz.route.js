5d4dd12ab374510671f576d685c71521718ed7c9
Tivanaht04 <101941055+GodjiMomonosuke@users.noreply.github.com>
2024-01-18 23:29:42 +0700

    Revert "update"
    This reverts commit 66f08cfb4a4ae9e97796d6f4c7bd264c946e5626.

--- routes/student/quiz/2_Datatype_and_Variable-quiz.route.js
+++ routes/student/quiz/2_Datatype_and_Variable-quiz.route.js
@@ -47,103 +47,27 @@ router.post('/submit', async (req, res, next) => {
   const choice1  = req.body.choice1
   const choice2  = req.body.choice2
   const choice3  = req.body.choice3
-  const choice4  = req.body.choice4
-  const choice5  = req.body.choice5
-  const choice6  = req.body.choice6
-  const choice7  = req.body.choice7
-  const choice8  = req.body.choice8
-  const choice9  = req.body.choice9
-  const choice10  = req.body.choice10
-  const choice11  = req.body.choice11
-  const choice12  = req.body.choice12
-  const choice13  = req.body.choice13
-  const choice14  = req.body.choice14
-  const choice15  = req.body.choice15
-  
-  var code = req.body.code;
+ var code = req.body.code;
   var lang = req.body.lang;
-  var expResult16 = req.body.expResult16
-  var expResult17 = req.body.expResult17
-  var expResult18 = req.body.expResult18
-  var expResult19 = req.body.expResult19
-  var expResult20 = req.body.expResult20
-  var sendLink16 = req.body.sendLink16
-  var sendLink171 = req.body.sendLink171
-  var sendLink172 = req.body.sendLink172
-  var sendLink181 = req.body.sendLink181
-  var sendLink182 = req.body.sendLink182
-  var sendLink191 = req.body.sendLink191
-  var sendLink192 = req.body.sendLink192
-  var sendLink201 = req.body.sendLink201
-  var sendLink202 = req.body.sendLink202
-
-  var score1 = 0;
-  var score2 = 0;
-  var score3 = 0;
-  var score4 = 0;
-  var score5 = 0;
-  var score6 = 0;
-  var score7 = 0;
-  var score8 = 0;
-  var score9 = 0;
-  var score10 = 0;
-  var score11 = 0;
-  var score12 = 0;
-  var score13 = 0;
-  var score14 = 0;
-  var score15 = 0;
-
-  var currentQuiz = "Datatype and Variable-Postest" //*** */
+  var expResult = req.body.expResult
+  var ImproveResult = ADRI_Expect
+  var Improvevariable = req.body.Improvevariable
+  var scoreLV1 = 0;
+  var scoreLV2 = 0;
+  var scoreLV3 = 0;
+  var currentQuiz = "String-Quiz" //*** */
   var timetodo = 0;
 
   /** chekc score */
-  if(choice1 === 'C'){
-    score1 = 1;
+  if(choice1 === 'D'){
+    scoreLV1 = 10;
   }
-  if(choice2 === 'C'){
-    score2 = 1;
+  if(choice2 === 'B'){
+    scoreLV2 = 20;
   }
-  if(choice3 === 'A'){
-    score3 = 1;
+  if(choice3 === 'right'){
+    scoreLV3 = 30;
   }
-  if(choice4 === 'C'){
-    score4 = 1;
-  }
-  if(choice5 === 'A'){
-    score5 = 1;
-  }
-  if(choice6 === 'B'){
-    score6 = 1;
-  }
-  if(choice7 === 'B'){
-    score7 = 1;
-  }
-  if(choice8 === 'A'){
-    score8 = 1;
-  }
-  if(choice9 === 'D'){
-    score9 = 1;
-  }
-  if(choice10 === 'C'){
-    score10 = 1;
-  }
-  if(choice11 === 'right'){
-    score11 = 1;//ตรวจคำตอบข้อ 3 ต้องไปที่ public\javascripts\lv3 (เรียงให้ถูก)
-  }
-  if(choice12 === 'A'){
-    score12 = 1;
-  }
-  if(choice13 === 'A'){
-    score13 = 1;
-  }
-  if(choice14 === 'D'){
-    score14 = 1;
-  }
-  if(choice15 === 'D'){
-    score15 = 1;
-  }
-
-
   /** compiler */
   var envData = { OS : "linux" , cmd : "gcc" };
   compiler.compileCPP(envData , code , function (data) {
@@ -174,28 +98,17 @@ router.post('/submit', async (req, res, next) => {
                 email: person.email,
                 role:person.role,
                 contentName:currentQuiz,
-                scoreLV1:score1+score2+score3+score4+score5,
-                scoreLV2:score6+score7+score8+score9+score10,
-                scoreLV3:score11+score12+score13+score14+score15,
+                scoreLV1:scoreLV1,
+                scoreLV2:scoreLV2,
+                scoreLV3:scoreLV3,
                 lang:lang,
                 code:code,
                 output:dataOut,
-                expResult16:expResult16,
-                expResult17:expResult17,
-                expResult18:expResult18,
-                expResult19:expResult19,
-                expResult20:expResult20,
-                sendLink16:sendLink16,
-                sendLink171:sendLink171,
-                sendLink172:sendLink172,
-                sendLink181:sendLink181,
-                sendLink182:sendLink182,
-                sendLink191:sendLink191,
-                sendLink192:sendLink192,
-                sendLink201:sendLink201,
-                sendLink202:sendLink202,
-                /**ADRI:ADRI,
-                ADRI_Answer:ADRI_Answer*/
+                ImproveResult:ImproveResult,
+                Improvevariable:Improvevariable,
+                expResult:expResult,
+                ADRI:ADRI,
+                ADRI_Answer:ADRI_Answer
               };
               dbo.collection("StudentAnswer").insertOne(myobj, function(err, res) {
                 if (err) throw err;
