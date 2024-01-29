88eb8ecc02c47d68d7d87bc57d3193bb2075008a
Tivanaht04 <101941055+GodjiMomonosuke@users.noreply.github.com>
2024-01-29 21:55:04 +0700

    what?

--- public/javascripts/gamequiz.js
+++ public/javascripts/gamequiz.js
@@ -26,6 +26,41 @@ function myFunctionW6() {document.getElementById("demo6").innerHTML = "Wrong T~T
 function myFunctionT7() {document.getElementById("demo7").innerHTML = "Right ^~^";}
 function myFunctionW7() {document.getElementById("demo7").innerHTML = "Wrong T~T";}
 
+//** 8 **/
+function myFunctionT8() {document.getElementById("demo8").innerHTML = "Right ^~^";}
+function myFunctionW8() {document.getElementById("demo8").innerHTML = "Wrong T~T";}
+
+//** 9 **/
+function myFunctionT9() {document.getElementById("demo9").innerHTML = "Right ^~^";}
+function myFunctionW9() {document.getElementById("demo9").innerHTML = "Wrong T~T";}
+
+//** 10 **/
+function myFunctionT10() {document.getElementById("demo10").innerHTML = "Right ^~^";}
+function myFunctionW10() {document.getElementById("demo10").innerHTML = "Wrong T~T";}
+
+//** 11 **/
+function myFunctionT11() {document.getElementById("demo11").innerHTML = "Right ^~^";}
+function myFunctionW11() {document.getElementById("demo11").innerHTML = "Wrong T~T";}
+
+//** 12 **/
+function myFunctionT12() {document.getElementById("demo12").innerHTML = "Right ^~^";}
+function myFunctionW12() {document.getElementById("demo12").innerHTML = "Wrong T~T";}
+
+//** 13 **/
+function myFunctionT13() {document.getElementById("demo13").innerHTML = "Right ^~^";}
+function myFunctionW13() {document.getElementById("demo13").innerHTML = "Wrong T~T";}
+
+//** 14 **/
+function myFunctionT14() {document.getElementById("demo14").innerHTML = "Right ^~^";}
+function myFunctionW14() {document.getElementById("demo14").innerHTML = "Wrong T~T";}
+
+//** 15 **/
+function myFunctionT15() {document.getElementById("demo15").innerHTML = "Right ^~^";}
+function myFunctionW15() {document.getElementById("demo15").innerHTML = "Wrong T~T";}
+
+//** 16 **/
+function myFunctionT16() {document.getElementById("demo16").innerHTML = "Right ^~^";}
+function myFunctionW16() {document.getElementById("demo16").innerHTML = "Wrong T~T";}
 
 function myFunction() {
     var x = document.getElementById("myDIV");
