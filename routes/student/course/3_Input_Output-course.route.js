297c4e8f9b0c40bf75c6aac330b71256e00f2b5a
Meenssi <sirapatson.imkr@bumail.net>
2024-01-30 23:10:52 +0700

    update

--- routes/student/course/3_Input_Output-course.route.js
+++ routes/student/course/3_Input_Output-course.route.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res, next) => {
       dbo.collection("StudentRecommendation").find(query).toArray(function(err, RecommendaResult) {
         if (err) throw err;
     
-        res.render('student/course/3_Datatype-course', { person ,RecommendaResult });
+        res.render('student/course/3_Input_Output-course', { person ,RecommendaResult });
       });
     });
 
