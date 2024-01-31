d735b811182596cde62a6d933fb9310b391f4f66
Meenssi <sirapatson.imkr@bumail.net>
2024-01-31 20:36:06 +0700

    Update 4_Operators_and_Expressions-quiz.route.js

--- routes/student/quiz/4_Operators_and_Expressions-quiz.route.js
+++ routes/student/quiz/4_Operators_and_Expressions-quiz.route.js
@@ -6,9 +6,11 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://admin:1234@cluster0.ormtjkb.mongodb.net";
 const mydatabase = "Cluster0";
 
-var ADRI = "https://drive.google.com/file/d/1e-4WHpl3dtY40hbIo8J_BovTjsMu1kw_/preview"
-var ADRI_Expect = "เขียนโปรแกรมหากำไรที่นาย ก ไปลงทุนใน dogecoin เหรียญละ 3 บาท เป็นจำนวน 1,200 เหรียญ เมื่อผ่านไป 1 ปี นาย ก ได้เงินลงทุนกลับคืนมา 4,140 บาท นาย ก ได้กำไรกี่เปอร์เซ็น"
-var ADRI_Answer = "https://drive.google.com/file/d/11o932LUbVIl33soe5-pvQIYpnVSxAmAD/preview"
+var ADRI1 = "https://drive.google.com/file/d/1pBIrINpZGRsPmonHCMpD6Vrobal8vX5q/preview"
+var ADRI2 = "https://drive.google.com/file/d/1o0XCMzQm7WUNV639rhytCrxdZaYoLbuw/preview"
+var ADRI3 = "https://drive.google.com/file/d/1CX7kBXlcwnKMsxnXpHWhnEWqrS51hWxc/preview"
+var ADRI4 = "https://drive.google.com/file/d/1O_bv7V3TGVRSRU5wWClBmtI4fcOfZ5ia/preview"
+var ADRI5 = "https://drive.google.com/file/d/11TahBnBjiHp-YtvHoeBw2eblzJECsOra/preview"
 
 router.get('/', async (req, res, next) => {
   const person = req.user;
@@ -19,8 +21,8 @@ router.get('/', async (req, res, next) => {
     var query = { email: person.email };
     dbo.collection("StudentAnswer").find(query).toArray(function(err, StudentAnswer) {
       if (err) throw err;
-      if(Object.keys(StudentAnswer).length === 0){
-        res.redirect('/')
+      if(Object.keys(StudentAnswer).length < 6){
+        res.redirect('/pretest3_check')
       }
       else{
         MongoClient.connect(url, function(err, db) {
@@ -30,7 +32,7 @@ router.get('/', async (req, res, next) => {
           dbo.collection("StudentRecommendation").find(query).toArray(function(err, RecommendaResult) {
             if (err) throw err;
 
-            res.render('student/quiz/4_Operators-quiz', { person ,StudentAnswer,RecommendaResult,ADRI,ADRI_Expect});
+            res.render('student/quiz/4_Operators_and_Expressions-quiz', { person ,StudentAnswer,RecommendaResult,ADRI1,ADRI2,ADRI3,ADRI4,ADRI5});
           });
         });
       }
@@ -40,33 +42,110 @@ router.get('/', async (req, res, next) => {
   // PRETEST Check
 });
 
+
 /** user quiz send  */
 router.post('/submit', async (req, res, next) => {
   const person = req.user;
   const choice1  = req.body.choice1
   const choice2  = req.body.choice2
   const choice3  = req.body.choice3
- var code = req.body.code;
+  const choice4  = req.body.choice4
+  const choice5  = req.body.choice5
+  const choice6  = req.body.choice6
+  const choice7  = req.body.choice7
+  const choice8  = req.body.choice8
+  const choice9  = req.body.choice9
+  const choice10  = req.body.choice10
+  const choice11  = req.body.choice11
+  const choice12  = req.body.choice12
+  const choice13  = req.body.choice13
+  const choice14  = req.body.choice14
+  const choice15  = req.body.choice15
+  
+  var code = req.body.code;
   var lang = req.body.lang;
-  var expResult = req.body.expResult
-  var ImproveResult = ADRI_Expect
-  var Improvevariable = req.body.Improvevariable
-  var scoreLV1 = 0;
-  var scoreLV2 = 0;
-  var scoreLV3 = 0;
-  var currentQuiz = "Operators-Quiz" //*** */
+  var expResult16 = req.body.expResult16
+  var expResult17 = req.body.expResult17
+  var expResult18 = req.body.expResult18
+  var expResult19 = req.body.expResult19
+  var expResult20 = req.body.expResult20
+  var sendLink16 = req.body.sendLink16
+  var sendLink171 = req.body.sendLink171
+  var sendLink172 = req.body.sendLink172
+  var sendLink181 = req.body.sendLink181
+  var sendLink182 = req.body.sendLink182
+  var sendLink191 = req.body.sendLink191
+  var sendLink192 = req.body.sendLink192
+  var sendLink201 = req.body.sendLink201
+  var sendLink202 = req.body.sendLink202
+
+  var score1 = 0;
+  var score2 = 0;
+  var score3 = 0;
+  var score4 = 0;
+  var score5 = 0;
+  var score6 = 0;
+  var score7 = 0;
+  var score8 = 0;
+  var score9 = 0;
+  var score10 = 0;
+  var score11 = 0;
+  var score12 = 0;
+  var score13 = 0;
+  var score14 = 0;
+  var score15 = 0;
+
+  var currentQuiz = "Operators_and_Expressions-(Post-test)" //*** */
   var timetodo = 0;
 
-  /** chekc score */
-  if(choice1 === 'D'){
-    scoreLV1 = 10;
+  /** check score */
+  if(choice1 === 'A'){
+    score1 = 1;
   }
   if(choice2 === 'D'){
-    scoreLV2 = 20;
+    score2 = 1;
   }
-  if(choice3 === 'right'){
-    scoreLV3 = 30;
+  if(choice3 === 'C'){
+    score3 = 1;
   }
+  if(choice4 === 'A'){
+    score4 = 1;
+  }
+  if(choice5 === 'C'){
+    score5 = 1;
+  }
+  if(choice6 === 'C'){
+    score6 = 1;
+  }
+  if(choice7 === 'C'){
+    score7 = 1;
+  }
+  if(choice8 === 'B'){
+    score8 = 1;
+  }
+  if(choice9 === 'A'){
+    score9 = 1;
+  }
+  if(choice10 === 'B'){
+    score10 = 1;
+  }
+  if(choice11 === 'A'){
+    score11 = 1;
+  }
+  if(choice12 === 'D'){
+    score12 = 1;
+  }
+  if(choice13 === 'B'){
+    score13 = 1;
+  }
+  if(choice14 === 'C'){
+    score14 = 1;
+  }
+  if(choice15 === 'B'){
+    score15 = 1;
+  }
+
+
   /** compiler */
   var envData = { OS : "linux" , cmd : "gcc" };
   compiler.compileCPP(envData , code , function (data) {
@@ -95,19 +174,47 @@ router.post('/submit', async (req, res, next) => {
                 timetodo:timetodo+1,
                 times: new Date().toLocaleString(), 
                 email: person.email,
+                Name: person.name,
+                studentID: person.studentID,
                 role:person.role,
                 contentName:currentQuiz,
-                scoreLV1:scoreLV1,
-                scoreLV2:scoreLV2,
-                scoreLV3:scoreLV3,
+                c1:score1,
+                c2:score2,
+                c3:score3,
+                c4:score4,
+                c5:score5,
+                scoreLV1:score1+score2+score3+score4+score5,
+                c6:score6,
+                c7:score7,
+                c8:score8,
+                c9:score9,
+                c10:score10,
+                scoreLV2:score6+score7+score8+score9+score10,
+                c11:score11,
+                c12:score12,
+                c13:score13,
+                c14:score14,
+                c15:score15,
+                scoreLV3:score11+score12+score13+score14+score15,
                 lang:lang,
                 code:code,
                 output:dataOut,
-                ImproveResult:ImproveResult,
-                Improvevariable:Improvevariable,
-                expResult:expResult,
-                ADRI:ADRI,
-                ADRI_Answer:ADRI_Answer
+                expResult16:expResult16,
+                sendLink16:sendLink16,
+                expResult17:expResult17,
+                sendLink171:sendLink171,
+                sendLink172:sendLink172,
+                expResult18:expResult18,
+                sendLink181:sendLink181,
+                sendLink182:sendLink182,
+                expResult19:expResult19,
+                sendLink191:sendLink191,
+                sendLink192:sendLink192,
+                expResult20:expResult20,
+                sendLink201:sendLink201,
+                sendLink202:sendLink202,
+                /**ADRI:ADRI,
+                ADRI_Answer:ADRI_Answer*/
               };
               dbo.collection("StudentAnswer").insertOne(myobj, function(err, res) {
                 if (err) throw err;
