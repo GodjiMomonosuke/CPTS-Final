0367e94e3e7499dd09546de95f1be8e90a0cfb25
Meenssi <sirapatson.imkr@bumail.net>
2024-02-04 11:08:07 +0700

    update แบบทดสอบบท 4
    pre-test
    post-test

--- routes/student/pretest_check/pretest4_check.route.js
+++ routes/student/pretest_check/pretest4_check.route.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res, next) => {
       dbo.collection("StudentRecommendation").find(query).toArray(function(err, RecommendaResult) {
         if (err) throw err;
     
-        res.render('student/pretest_check/pretest3_check', { person ,RecommendaResult });
+        res.render('student/pretest_check/pretest4_check', { person ,RecommendaResult });
       });
     });
 
