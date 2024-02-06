b05ae975b87ff88a4abce35c4d665896db0aff00
Meenssi <sirapatson.imkr@bumail.net>
2024-02-06 21:57:47 +0700

    Update gamequiz.js

--- public/javascripts/gamequiz.js
+++ public/javascripts/gamequiz.js
@@ -1,134 +1,92 @@
 //** 1 **/
-function myFunctionT1() {document.getElementById("demo1").innerHTML = "✅ จำนวนเต็ม ";}
+function myFunctionT1() {document.getElementById("demo1").innerHTML = "✅✅✅";}
 function myFunctionW1() {document.getElementById("demo1").innerHTML = "❌❌❌";}
 
 //** 2 **/
-function myFunctionT2() {document.getElementById("demo2").innerHTML = "✅ 26.83 ";}
+function myFunctionT2() {document.getElementById("demo2").innerHTML = "✅✅✅";}
 function myFunctionW2() {document.getElementById("demo2").innerHTML = "❌❌❌";}
 
 //** 3 **/
-function myFunctionT3() {document.getElementById("demo3").innerHTML = "✅ ตัวอักขระ ";}
+function myFunctionT3() {document.getElementById("demo3").innerHTML = "✅✅✅";}
 function myFunctionW3() {document.getElementById("demo3").innerHTML = "❌❌❌";}
 
 //** 4 **/
-function myFunctionT4() {document.getElementById("demo4").innerHTML = "✅ double x = 10.5; ";}
+function myFunctionT4() {document.getElementById("demo4").innerHTML = "✅✅✅";}
 function myFunctionW4() {document.getElementById("demo4").innerHTML = "❌❌❌";}
 
 //** 5 **/
-function myFunctionT5() {document.getElementById("demo5").innerHTML = "✅ Global Variable";}
+function myFunctionT5() {document.getElementById("demo5").innerHTML = "✅✅✅";}
 function myFunctionW5() {document.getElementById("demo5").innerHTML = "❌❌❌";}
 
 //** 6 **/
-function myFunctionT6() {document.getElementById("demo6").innerHTML = "✅ Local Variable";}
+function myFunctionT6() {document.getElementById("demo6").innerHTML = "✅✅✅";}
 function myFunctionW6() {document.getElementById("demo6").innerHTML = "❌❌❌";}
 
 //** 7 **/
-function myFunctionT7() {document.getElementById("demo7").innerHTML = "✅ 30";}
+function myFunctionT7() {document.getElementById("demo7").innerHTML = "✅✅✅";}
 function myFunctionW7() {document.getElementById("demo7").innerHTML = "❌❌❌";}
 
 //** 8 **/
-function myFunctionT8() {document.getElementById("demo8").innerHTML = "✅ Error";}
+function myFunctionT8() {document.getElementById("demo8").innerHTML = "✅✅✅";}
 function myFunctionW8() {document.getElementById("demo8").innerHTML = "❌❌❌";}
 
 //** 9 **/
-function myFunctionT9() {document.getElementById("demo9").innerHTML = "✅ โปรแกรมจะเกิดข้อผิดพลาดในการคอมไพล์เนื่องจากพยายามเปลี่ยนค่าตัวแปร z ที่เป็น const ";}
+function myFunctionT9() {document.getElementById("demo9").innerHTML = "✅✅✅";}
 function myFunctionW9() {document.getElementById("demo9").innerHTML = "❌❌❌";}
 
 //** 10 **/
-function myFunctionT10() {document.getElementById("demo10").innerHTML = "✅ โปรแกรมกำหนดค่าคงที่ MAX_VALUE เป็น 100, ประกาศตัวแปรค่าคงที่ x เป็น 80 และ z เป็น 'A' แล้วแสดงผลลัพธ์ 80, A, 100 ";}
+function myFunctionT10() {document.getElementById("demo10").innerHTML = "✅✅✅";}
 function myFunctionW10() {document.getElementById("demo10").innerHTML = "❌❌❌";}
 
-
-//แบบฝึกหัดของบที่ 3
-//** 1 **/
-function myFunctionEP3T1() {document.getElementById("demo3_1").innerHTML = "✅";}
-function myFunctionEP3W1() {document.getElementById("demo3_1").innerHTML = "❌❌❌";}
-
-//** 2 **/
-function myFunctionEP3T2() {document.getElementById("demo3_2").innerHTML = "✅";}
-function myFunctionEP3W2() {document.getElementById("demo3_2").innerHTML = "❌❌❌";}
-
-//** 3 **/
-function myFunctionEP3T3() {document.getElementById("demo3_3").innerHTML = "✅";}
-function myFunctionEP3W3() {document.getElementById("demo3_3").innerHTML = "❌❌❌";}
-
-//** 4 **/
-function myFunctionEP3T4() {document.getElementById("demo3_4").innerHTML = "✅";}
-function myFunctionEP3W4() {document.getElementById("demo3_4").innerHTML = "❌❌❌";}
-
-//** 5 **/
-function myFunctionEP3T5() {document.getElementById("demo3_5").innerHTML = "✅";}
-function myFunctionEP3W5() {document.getElementById("demo3_5").innerHTML = "❌❌❌";}
-
-//** 6 **/
-function myFunctionEP3T6() {document.getElementById("demo3_6").innerHTML = "✅";}
-function myFunctionEP3W6() {document.getElementById("demo3_6").innerHTML = "❌❌❌";}
-
-//** 7 **/
-function myFunctionEP3T7() {document.getElementById("demo3_7").innerHTML = "✅";}
-function myFunctionEP3W7() {document.getElementById("demo3_7").innerHTML = "❌❌❌";}
-
-//** 8 **/
-function myFunctionEP3T8() {document.getElementById("demo3_8").innerHTML = "✅";}
-function myFunctionEP3W8() {document.getElementById("demo3_8").innerHTML = "❌❌❌";}
-
-//** 9 **/
-function myFunctionEP3T9() {document.getElementById("demo3_9").innerHTML = "✅";}
-function myFunctionEP3W9() {document.getElementById("demo3_9").innerHTML = "❌❌❌";}
-
-//** 10 **/
-function myFunctionEP3T10() {document.getElementById("demo3_10").innerHTML = "✅";}
-function myFunctionEP3W10() {document.getElementById("demo3_10").innerHTML = "❌❌❌";}
-
-//Tracing 
+//***** Tracing *****//
 //** 11 **/
-function myFunctionEP3T11() {document.getElementById("demo3_11").innerHTML = "✅";}
-function myFunctionEP3W11() {document.getElementById("demo3_11").innerHTML = "❌❌❌";}
+function myFunctionT11() {document.getElementById("demo11").innerHTML = "✅✅✅";}
+function myFunctionW11() {document.getElementById("demo11").innerHTML = "❌❌❌";}
 
 //** 12 **/
-function myFunctionEP3T12() {document.getElementById("demo3_12").innerHTML = "✅";}
-function myFunctionEP3W12() {document.getElementById("demo3_12").innerHTML = "❌❌❌";}
+function myFunctionT12() {document.getElementById("demo12").innerHTML = "✅✅✅";}
+function myFunctionW12() {document.getElementById("demo12").innerHTML = "❌❌❌";}
 
 //** 13 **/
-function myFunctionEP3T13() {document.getElementById("demo3_13").innerHTML = "✅";}
-function myFunctionEP3W13() {document.getElementById("demo3_13").innerHTML = "❌❌❌";}
+function myFunctionT13() {document.getElementById("demo13").innerHTML = "✅✅✅";}
+function myFunctionW13() {document.getElementById("demo13").innerHTML = "❌❌❌";}
 
 //** 14 **/
-function myFunctionEP3T14() {document.getElementById("demo3_14").innerHTML = "✅";}
-function myFunctionEP3W14() {document.getElementById("demo3_14").innerHTML = "❌❌❌";}
+function myFunctionT14() {document.getElementById("demo14").innerHTML = "✅✅✅";}
+function myFunctionW14() {document.getElementById("demo14").innerHTML = "❌❌❌";}
 
 //** 15 **/
-function myFunctionEP3T15() {document.getElementById("demo3_15").innerHTML = "✅";}
-function myFunctionEP3W15() {document.getElementById("demo3_15").innerHTML = "❌❌❌";}
+function myFunctionT15() {document.getElementById("demo15").innerHTML = "✅✅✅";}
+function myFunctionW15() {document.getElementById("demo15").innerHTML = "❌❌❌";}
 
 //** 16 **/
-function myFunctionEP3T16() {document.getElementById("demo3_16").innerHTML = "✅";}
-function myFunctionEP3W16() {document.getElementById("demo3_16").innerHTML = "❌❌❌";}
+function myFunctionT16() {document.getElementById("demo16").innerHTML = "✅✅✅";}
+function myFunctionW16() {document.getElementById("demo16").innerHTML = "❌❌❌";}
 
 //** 17 **/
-function myFunctionEP3T17() {document.getElementById("demo3_17").innerHTML = "✅";}
-function myFunctionEP3W17() {document.getElementById("demo3_17").innerHTML = "❌❌❌";}
+function myFunctionT17() {document.getElementById("demo17").innerHTML = "✅✅✅";}
+function myFunctionW17() {document.getElementById("demo17").innerHTML = "❌❌❌";}
 
 //** 18 **/
-function myFunctionEP3T18() {document.getElementById("demo3_18").innerHTML = "✅";}
-function myFunctionEP3W18() {document.getElementById("demo3_18").innerHTML = "❌❌❌";}
+function myFunctionT18() {document.getElementById("demo18").innerHTML = "✅✅✅";}
+function myFunctionW18() {document.getElementById("demo18").innerHTML = "❌❌❌";}
 
 //** 19 **/
-function myFunctionEP3T19() {document.getElementById("demo3_19").innerHTML = "✅";}
-function myFunctionEP3W19() {document.getElementById("demo3_19").innerHTML = "❌❌❌";}
+function myFunctionT19() {document.getElementById("demo19").innerHTML = "✅✅✅";}
+function myFunctionW19() {document.getElementById("demo19").innerHTML = "❌❌❌";}
 
 //** 20 **/
-function myFunctionEP3T20() {document.getElementById("demo3_20").innerHTML = "✅";}
-function myFunctionEP3W20() {document.getElementById("demo3_20").innerHTML = "❌❌❌";}
+function myFunctionT20() {document.getElementById("demo20").innerHTML = "✅✅✅";}
+function myFunctionW20() {document.getElementById("demo20").innerHTML = "❌❌❌";}
 
 //Explaining 
 //** 21 **/
-function myFunctionEP3T21() {document.getElementById("demo3_21").innerHTML = "✅";}
-function myFunctionEP3W21() {document.getElementById("demo3_21").innerHTML = "❌❌❌";}
+function myFunctionT21() {document.getElementById("demo21").innerHTML = "✅✅✅";}
+function myFunctionW21() {document.getElementById("demo21").innerHTML = "❌❌❌";}
 
 //** 22 **/
-function myFunctionEP3T22() {document.getElementById("demo3_22").innerHTML = "✅";}
-function myFunctionEP3W22() {document.getElementById("demo3_22").innerHTML = "❌❌❌";}
+function myFunctionT22() {document.getElementById("demo22").innerHTML = "✅✅✅";}
+function myFunctionW22() {document.getElementById("demo22").innerHTML = "❌❌❌";}
 
 
 
