d7856f1510647f565d1a923941d74877db2093bb
Meenssi <sirapatson.imkr@bumail.net>
2025-03-01 16:16:30 +0700

    อัพเดท Project ฝั่งอาจารย์

---
+++ routes/teacher/TeacherProject/1_TicketBookingSystem.route.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://cpts9850:Cpts1234@cluster0.fvblynd.mongodb.net";
+const mydatabase = "Cluster0";
+
+router.get('/', async (req, res, next) => {
+  const person = req.user;
+  if(person != undefined){
+
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db(mydatabase);
+      var query = {email:person.email};
+      dbo.collection("StudentRecommendation").find(query).toArray(function(err) {
+        if (err) throw err;
+    
+        res.render('teacher/TeacherProject/1_TicketBookingSystem', { person });
+      });
+    });
+
+     
+  }
+});
+
+
+
+module.exports = router;
+
+
+
+
+
+
